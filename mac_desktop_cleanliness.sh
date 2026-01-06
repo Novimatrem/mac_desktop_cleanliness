cb20b2e60386d4ce343292736950581e2b6c5013
Novimatrem <thenovimatrem@protonmail.ch>
2026-01-06 19:02:27 +0000

    multicommittr sync

--- mac_desktop_cleanliness.sh
+++ mac_desktop_cleanliness.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+defaults write com.apple.finder CreateDesktop false
+
+sleep 1s
+
+killall Finder
+
+sleep 1s
+
 while true
 do
 
 cd /Users/$(whoami)
 mkdir /Users/$(whoami)/Desktop2
 rsync -av Desktop/ Desktop2 --remove-source-files
-
+defaults write com.apple.finder CreateDesktop false
 sleep 1s # prevent lag by doing this too often
 
 done
