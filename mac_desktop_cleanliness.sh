05f8fbff5efe955cdaec1893d7dbc595445a4413
Novimatrem <69379708+Novimatrem@users.noreply.github.com>
2026-08-02 13:46:01 +0100

    hard code to fix for now

--- mac_desktop_cleanliness.sh
+++ mac_desktop_cleanliness.sh
@@ -13,8 +13,8 @@ sleep 1s
 while true
 do
 
-cd /Users/$(whoami)
-mkdir /Users/$(whoami)/Desktop2
+cd /Users/zoeyglobe
+mkdir /Users/zoeyglobe/Desktop2
 rsync -av Desktop/ Desktop2 --remove-source-files
 defaults write com.apple.finder CreateDesktop false
 sleep 1s # prevent lag by doing this too often
